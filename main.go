95f04580bdb222deaa377d8dea4b26919e95adab
Mekyle <mekylenaidoo@yahoo.com>
2018-08-11 07:49:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+
+  "github.com/gin-gonic/contrib/static"
+  "github.com/gin-gonic/gin"
 )
 
 // Handler is executed by AWS Lambda in the main function. Once the request
@@ -27,5 +31,23 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 }
 
 func main() {
-	lambda.Start(Handler)
+  lambda.Start(Handler)
+  // Set the router as the default one shipped with Gin
+  router := gin.Default()
+
+  // Serve frontend static files
+  router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+  // Setup route group for the API
+  api := router.Group("/api")
+  {
+    api.GET("/", func(c *gin.Context) {
+      c.JSON(http.StatusOK, gin.H {
+        "message": "pong",
+      })
+    })
+  }
+
+  // Start and run the server
+  router.Run(":3000")
 }
